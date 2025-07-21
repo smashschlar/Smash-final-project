31a4a3f38781074e466e07356895815b7c8d3fbb
smashschlar <ali.kasbour1@gmail.com>
2025-07-21 00:47:23 +0000

    Edited files

--- pages/JANZBOT.py
+++ pages/JANZBOT.py
@@ -1,22 +1,22 @@
-import openai
+# import openai
 
-openai.api_key = "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"
 
-def chat_with_gpt(prompt):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ]
-    )
-    return response.choices[0].message.contect.stip()
 
-if __name__ == "__main__":
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() in ["quit", "exit", "bye"]:
-            break 
+# def chat_with_gpt(prompt):
+#     response = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[
+#             {"role": "system", "content": "You are a helpful assistant."},
+#             {"role": "user", "content": prompt}
+#         ]
+#     )
+#     return response.choices[0].message.contect.stip()
 
-        response = chat_with_gpt(user_input)
-        print("Chatbot", response)
+# if __name__ == "__main__":
+#     while True:
+#         user_input = input("You: ")
+#         if user_input.lower() in ["quit", "exit", "bye"]:
+#             break 
+
+#         response = chat_with_gpt(user_input)
+#         print("Chatbot", response)
